1bdde7c301e20a13a205c11468c0a19ff0050510
Dementyev Alexey <adementyev@zaoblako.ru>
2018-02-27 18:56:11 +0300

    fix build

---
+++ locales/en-GB.js
@@ -0,0 +1,56 @@
+var enGB = {
+    datepicker: {
+        months: [
+            "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+        ],
+        monthsShort: [
+            "Янв", "Февр", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сент", "Окт", "Ноя", "Дек"
+        ],
+        weekdays: [
+            "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"
+        ],
+        weekdaysShort: [
+            "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"
+        ],
+        weekdaysNarrow: [
+            "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"
+        ],
+        timesOfDay: [
+            "", ""
+        ],
+        timesOfDayUppercase: [
+            "", ""
+        ],
+        timesOfDayLowercase: [
+            "", ""
+        ],
+        formats: {
+            time: "HH:mm",
+            datetime: "DD.MM.YYYY HH:mm",
+            date: "DD.MM.YYYY",
+            month: "MMMM YYYY",
+            year: "YYYY"
+        },
+        firstDayOfWeek: 1
+    },
+    search: {
+        placeholder: "Поиск...",
+        noResults: {
+            header: "Нет результатов",
+            message: "Поиск не дал результатов."
+        }
+    },
+    select: {
+        noResultsMessage: "Нет результатов",
+        single: {
+            placeholder: "Выберите одно значение"
+        },
+        multi: {
+            placeholder: "Выберите...",
+            maxSelectedMessage: "Не более #{max} значений",
+            selectedMessage: "выбрано #{count}"
+        }
+    }
+};
+export default enGB;
+//# sourceMappingURL=en-GB.js.map
---
+++ locales/ru.d.ts
@@ -0,0 +1,7 @@
+import { ILocaleValues } from "../dist";
+/**
+ * locale : Russian (ru)
+ * author : Maksim Moiseikin : https://github.com/maksim-m
+ */
+declare const ru: ILocaleValues;
+export default ru;
